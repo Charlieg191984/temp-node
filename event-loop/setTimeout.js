ad49ad98f4493754d7005753fb2059f1386f9a3f
Charlie <cgiraldo1919@gmail.com>
2023-06-12 14:07:05 -0400

    Updated app

---
+++ event-loop/setTimeout.js
@@ -0,0 +1,8 @@
+// started operating system process
+console.log("started first task");
+setTimeout(() => {
+  console.log("second task started");
+}, 0);
+
+console.log("third task started");
+// completed and exited operating system process
